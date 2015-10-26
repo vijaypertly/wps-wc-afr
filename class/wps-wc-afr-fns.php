e3e9c2f81f23c160526a89b6dbfca41d6c067d7e
Vijay M <vijay@pertly.co.in>
2015-10-26 21:25:15 +0530

    Worked on cron

--- class/wps-wc-afr-fns.php
+++ class/wps-wc-afr-fns.php
@@ -3,6 +3,7 @@ defined( 'ABSPATH' ) or die('');
 
 class WpsWcAFRFns{
     private static $logFile = '';
+    private static $templateDetails = array();
 
     public static function activatePlugin(){
         /*
@@ -24,11 +25,12 @@ class WpsWcAFRFns{
         $settings = get_option('wps_wc_afr_settings');
         if(empty($settings)){
             $settings = array(
-                'enable_cron'=> false,
+                /*'enable_cron'=> false,*/
+                'enable_cron'=> true,
                 'cron_time_in_minutes'=> 15,
                 'abandoned_time_in_minutes'=> 15,
             );
-            $settings = serialize($settings);
+            $settings = $settings;
         }
 
         return $settings;
@@ -59,10 +61,419 @@ class WpsWcAFRFns{
          * 1. Identifying and separating abandoned, failed, cancelled orders and inserting in mail queue.
          *      a. Identify
          *      b. change status to processed after processing record from table "wp_wps_wcafr"
-         *             1. Cases (Multiple emails)
+         *             1.
          * */
         self::debugLog('Started cron');
+        $settings = self::getSettings();
+
+        if($settings['enable_cron'] == true){
+            self::debugLog('Cron enabled.');
+            $activeRows = self::activeWpsRows();
+            if(!empty($activeRows)){
+                $totalActiveRows = count($activeRows);
+                if($totalActiveRows>0){
+                    foreach($activeRows as $activeRow){
+                        self::processRow($activeRow);
+                    }
+                }
+                self::debugLog('Total Active rows: '.$totalActiveRows);
+            }
+            else{
+                self::debugLog('No active rows found.');
+            }
+
+            self::processMailQueue();
+        }
+        else{
+            self::debugLog('Cron disabled.');
+        }
+
         self::debugLog('Stopped cron');
+    }
+
+    private static function processMailQueue(){
+        global $wpdb;
+        self::debugLog('Starting processing mail queue.');
+
+        $mailQueue = self::getMailQueue();
+        if(!empty($mailQueue)){
+            foreach($mailQueue as $mail){
+                if(!empty($mail['send_to_email'])){
+                    $arrParams = array(
+                        'to'=>$mail['send_to_email'],
+                        'subject'=>$mail['subject'],
+                        'message'=>$mail['message'],
+                    );
+                    if(self::sendMail($arrParams)){
+                        //Mail sent
+                        $wpdb->update(
+                            $wpdb->prefix.'wps_wcafr_mail_log',
+                            array(
+                                'mail_status' => 3,
+                                'mail_sent_on' => date('Y-m-d H:i:s'),
+                            ),
+                            array( 'id' => $mail['id'] )
+                        );
+                    }
+                    else{
+                        //Unable to send mail.
+                        $wpdb->update(
+                            $wpdb->prefix.'wps_wcafr_mail_log',
+                            array(
+                                'mail_status' => 1,
+                                'mail_sent_on' => date('Y-m-d H:i:s'),
+                            ),
+                            array( 'id' => $mail['id'] )
+                        );
+                    }
+
+                    $templateDetails = self::templateDetails($mail['template_id']);
+                    $lastMailedForMinutes = !empty($templateDetails['send_mail_duration_in_minutes'])?$templateDetails['send_mail_duration_in_minutes']:0;
+                    $arrUpdtRw = array(
+                        'id'=> $mail['wp_wps_id'],
+                        'set'=> array(
+                            'mail_status'=>'mailed',
+                            'last_mailed_for_minutes'=>$lastMailedForMinutes,
+                        ),
+                    );
+                    self::updateRow($arrUpdtRw);
+                }
+            }
+        }
+
+        self::debugLog('Ended processing mail queue.');
+    }
+
+    private static function sendMail($arrParams = array()){
+        $isSent = false;
+
+        if(!empty($arrParams['to']) && !empty($arrParams['subject']) && !empty($arrParams['message'])){
+            /*
+             * Todo: Send mail
+             * $arrParams = array(
+                        'to'=>'',
+                        'subject'=>'',
+                        'message'=>'',
+                    );
+             * */
+            $isSent = true;
+        }
+
+        return $isSent;
+    }
+
+    private static function getMailQueue(){
+        global $wpdb;
+
+        $details = array();
+
+        $query = "SELECT *  FROM `".$wpdb->prefix."wps_wcafr_mail_log` WHERE `is_deleted` = '0' AND `mail_status`= '0' ORDER BY `created` ASC";
+        $results = $wpdb->get_results($query, ARRAY_A );
+        if(!empty($results['0'])){
+            $details = $results;
+        }
+
+        return $details;
+    }
+
+    public static function processRow($activeRow = array()){
+        if(!empty($activeRow)){
+
+            $templateDetails = self::selectTemplate($activeRow);
+            $templateId = !empty($templateDetails['id'])?$templateDetails['id']:0;
+
+            if(in_array($activeRow['status'], array('new', 'abandoned'))){
+                //New record, but abandoned.
+                if(!empty($templateId)){
+                    $arrAddMailParams = array(
+                        'wps_row_id'=>$activeRow['id'],
+                        'template_id'=>$templateId,
+                    );
+                    self::addToMailQueue($arrAddMailParams);
+
+                    $arrUpdtRw = array(
+                        'id'=> $activeRow['id'],
+                        'set'=> array(
+                            'mail_status'=>'in_mail_queue',
+                            'status'=>'abandoned',
+                        ),
+                    );
+                    self::updateRow($arrUpdtRw);
+                }
+                else{
+                    //No template found for selected cat. It seems expired with no use. So delete.
+                    self::deleteWpsRow($activeRow['id']);
+                }
+            }
+            else if(in_array($activeRow['status'], array('order_created', 'payment_pending'))){
+                //Payment pending
+                if(!empty($templateId)){
+                    $arrAddMailParams = array(
+                        'wps_row_id'=>$activeRow['id'],
+                        'template_id'=>$templateId,
+                    );
+                    self::addToMailQueue($arrAddMailParams);
+
+                    $arrUpdtRw = array(
+                        'id'=> $activeRow['id'],
+                        'set'=> array(
+                            'mail_status'=>'in_mail_queue',
+                        ),
+                    );
+                    self::updateRow($arrUpdtRw);
+                }
+                else{
+                    //No template found for selected cat. It seems expired with no use. So delete.
+                    self::deleteWpsRow($activeRow['id']);
+                }
+            }
+            else if(in_array($activeRow['status'], array('order_cancelled'))){
+                //Order cancelled
+                if(!empty($templateId)){
+                    $arrAddMailParams = array(
+                        'wps_row_id'=>$activeRow['id'],
+                        'template_id'=>$templateId,
+                    );
+                    self::addToMailQueue($arrAddMailParams);
+
+                    $arrUpdtRw = array(
+                        'id'=> $activeRow['id'],
+                        'set'=> array(
+                            'mail_status'=>'in_mail_queue',
+                        ),
+                    );
+                    self::updateRow($arrUpdtRw);
+                }
+                else{
+                    //No template found for selected cat. It seems expired with no use. So delete.
+                    self::deleteWpsRow($activeRow['id']);
+                }
+            }
+            else if(in_array($activeRow['status'], array('payment_failed'))){
+                //Payment failed
+                if(!empty($templateId)){
+                    $arrAddMailParams = array(
+                        'wps_row_id'=>$activeRow['id'],
+                        'template_id'=>$templateId,
+                    );
+                    self::addToMailQueue($arrAddMailParams);
+
+                    $arrUpdtRw = array(
+                        'id'=> $activeRow['id'],
+                        'set'=> array(
+                            'mail_status'=>'in_mail_queue',
+                        ),
+                    );
+                    self::updateRow($arrUpdtRw);
+                }
+                else{
+                    //No template found for selected cat. It seems expired with no use. So delete.
+                    self::deleteWpsRow($activeRow['id']);
+                }
+            }
+        }
+    }
+
+    private static function deleteWpsRow($rowId = 0){
+        if(!empty($rowId)){
+            $arrUpdtRw = array(
+                'id'=> $rowId,
+                'set'=> array(
+                    'status'=>'deleted',
+                ),
+            );
+            self::updateRow($arrUpdtRw);
+        }
+    }
+
+    private static function selectTemplate($activeRow = array()){
+        global $wpdb;
+        $templateDetails = array();
+
+        if(!empty($activeRow['id']) && !in_array($activeRow['id'], array('recovered', 'deleted')) ){
+            $lastMailedForMinutes = !empty($activeRow['last_mailed_for_minutes'])?$activeRow['last_mailed_for_minutes']:0;
+            if(in_array($activeRow['status'], array('new', 'abandoned'))){
+                $templateFor = 'abandoned_cart';
+            }
+            else if(in_array($activeRow['status'], array('order_created', 'payment_pending'))){
+                $templateFor = 'pending_payment';
+            }
+            else if(in_array($activeRow['status'], array('order_cancelled'))){
+                $templateFor = 'cancelled_payment';
+            }
+            else if(in_array($activeRow['status'], array('payment_failed'))){
+                $templateFor = 'failed_payment';
+            }
+
+            if(!empty($templateFor)){
+                $query = "SELECT * FROM `wp_wps_wcafr_templates` WHERE `send_mail_duration_in_minutes` >".$lastMailedForMinutes." AND `template_for` = '".$templateFor."' AND `send_mail_duration_in_minutes` <= TIMESTAMPDIFF(MINUTE, '".$activeRow['created']."', '".date('Y-m-d H:i:s')."') ORDER BY `send_mail_duration_in_minutes` ASC LIMIT 1 ";
+                $results = $wpdb->get_results($query, ARRAY_A );
+                if(!empty($results['0'])){
+                    $templateDetails = $results['0'];
+                }
+            }
+        }
+
+        return $templateDetails;
+    }
+
+    private static function getLastMailLog($forWpsId = 0){
+        global $wpdb;
+
+        $details = array();
+
+        if(!empty($forWpsId)){
+            $query = "SELECT *  FROM `".$wpdb->prefix."wps_wcafr_mail_log` WHERE `wp_wps_id` = '".$forWpsId."' AND `is_deleted` = '0' ORDER BY `created` DESC LIMIT 1";
+            $results = $wpdb->get_results($query, ARRAY_A );
+            if(!empty($results['0'])){
+                $details = $results['0'];
+            }
+        }
+
+        return $details;
+    }
+
+    private static function getUserDetails($userId = 0){
+        return get_user_meta($userId);
+    }
+
+    private static function addToMailQueue($arrParams = array()){
+        global $wpdb;
+
+        if(!empty($arrParams['wps_row_id']) && !empty($arrParams['template_id'])){
+            $rowDetails = self::rowDetails($arrParams['wps_row_id']);
+            if(!empty($rowDetails['user_email'])){
+
+                $userEmail = $rowDetails['user_email'];
+                $templateDetails = self::templateDetails($arrParams['template_id']);
+                if(!empty($templateDetails)){
+                    $userDetails = (!empty($rowDetails['user_id']))?self::getUserDetails($rowDetails['user_id']):array();
+                    $userFirstName = !empty($userDetails['first_name']['0'])?$userDetails['first_name']['0']:'';
+                    $userLastName = !empty($userDetails['last_name']['0'])?$userDetails['last_name']['0']:'';
+                    $arrReplace = array(
+                        '0'=>array(
+                            'replace_match'=>'{wps.firstname}',
+                            'replace_value'=>$userFirstName,
+                        ),
+                        '1'=>array(
+                            'replace_match'=>'{wps.lastname}',
+                            'replace_value'=>$userLastName,
+                        ),
+                        '2'=>array(
+                            'replace_match'=>'{wps.email}',
+                            'replace_value'=>$userEmail,
+                        ),
+                    );
+                    $templateSubject = self::replaceTemplateMess($templateDetails['template_subject'], $arrReplace);
+                    $templateMessage = self::replaceTemplateMess($templateDetails['template_message'], $arrReplace);
+                    $params = array(
+                        'replace_arr'=>$arrReplace,
+                    );
+
+                    if(!empty($templateMessage)){
+                        $wpdb->insert(
+                            $wpdb->prefix.'wps_wcafr_mail_log',
+                            array(
+                                'wp_wps_id' => $arrParams['wps_row_id'],
+                                'template_id' => $arrParams['template_id'],
+                                'subject' => $templateSubject,
+                                'message' => $templateMessage,
+                                'send_to_email' => $userEmail,
+                                'params' => json_encode($params),
+                                'created' => date('Y-m-d H:i:s'),
+                                'mail_status' => 0,
+                                'is_deleted' => 0,
+                            )
+                        );
+                        self::debugLog('Added to mail queue for wp_wps_id: '.$arrParams['wps_row_id']);
+                    }
+                }
+            }
+        }
+    }
+
+    private static function replaceTemplateMess($templateMessage = '', $arrReplace = array()){
+        if(!empty($arrReplace)){
+            foreach($arrReplace as $repl){
+                $thisKey = $repl['replace_match'];
+                $withThisVal = $repl['replace_value'];
+                if(!empty($thisKey)){
+                    $templateMessage = str_ireplace($thisKey, $withThisVal, $templateMessage);
+                }
+            }
+        }
+
+        return $templateMessage;
+    }
+
+    private static function templateDetails($templateId = 0 ){
+        global $wpdb;
+        $templateDetails = array();
+
+        if(isset(self::$templateDetails[$templateId])){
+            return self::$templateDetails[$templateId];
+        }
+
+        if(!empty($templateId)){
+            $results = $wpdb->get_results( "SELECT * FROM ".$wpdb->prefix."wps_wcafr_templates WHERE `id` = '".$templateId."' limit 1", ARRAY_A );
+            if(!empty($results['0'])){
+                $templateDetails = $results['0'];
+                self::$templateDetails[$templateId] = $templateDetails;
+            }
+        }
+
+        return $templateDetails;
+    }
+
+    private static function rowDetails($rowId = 0 ){
+        global $wpdb;
+        $rowDetails = array();
+
+        if(!empty($rowId)){
+            $results = $wpdb->get_results( "SELECT * FROM ".$wpdb->prefix."wps_wcafr WHERE `id` = '".$rowId."' limit 1", ARRAY_A );
+            if(!empty($results['0'])){
+                $rowDetails = $results['0'];
+            }
+        }
+
+        return $rowDetails;
+    }
+
+    private static function updateRow($arrParams = array()){
+        global $wpdb;
+
+        if(!empty($arrParams['id']) && !empty($arrParams['set']) ){
+            $arrSet = $arrParams['set'];
+            $arrWhere = array(
+                'id'=>$arrParams['id'],
+            );
+            if(!empty($arrParams['where'])){
+                $arrWhere = $arrParams['where'];
+            }
+
+            $wpdb->update(
+                $wpdb->prefix.'wps_wcafr',
+                $arrSet,
+                $arrWhere
+            );
+        }
+    }
+
+    public static function activeWpsRows(){
+        global $wpdb;
+        self::debugLog('Checking for active rows.');
+        $activeRows = array();
+
+        $settings = self::getSettings();
+
+        $query = "SELECT  * FROM `wp_wps_wcafr` WHERE `status` != 'deleted'  AND `status` != 'recovered'  AND `status` != 'order_processing' AND TIMESTAMPDIFF(MINUTE, `last_active_cart_added`, '".date('Y-m-d H:i:s')."') >= '".$settings['abandoned_time_in_minutes']."'";
+        //self::debugLog($query);
+        $results = $wpdb->get_results($query, ARRAY_A);
+        if(!empty($results)){
+            $activeRows = $results;
+        }
+
+        return $activeRows;
     }
 
     /*
@@ -142,7 +553,7 @@ class WpsWcAFRFns{
                     );
                 }
 
-                if(in_array($orderDetails['post_status'], array('wc-completed'))){
+                if(in_array($orderDetails['post_status'], array('wc-completed', 'wc-processing'))){
                     /*
                      * For completed orders
                      *  1. If already mail sent, then it is recovered order, else need to remove complete row from table.
@@ -153,6 +564,10 @@ class WpsWcAFRFns{
                         $isRecoveredOrder = false;
                         if($results['0']['mail_status']=='not_mailed'){
                             //Remove row. As, user itself ordered.
+                            $rmWhere = array(
+                                'id'=>$results['0']['id'],
+                            );
+                            $wpdb->delete( "".$wpdb->prefix."wps_wcafr", $rmWhere );
                         }
                         else if($results['0']['mail_status']=='mailed'){
                             //It is a recovered order
@@ -179,10 +594,18 @@ class WpsWcAFRFns{
                             ),
                             array( '%d' )
                         );
+
+                        self::wpsOrderRecovered($results['0']['id']);
                     }
                 }
 
             }
+        }
+    }
+
+    public static function wpsOrderRecovered($wpsId = 0){
+        if(!empty($wpsId)){
+            //Cart recovered after sending mails.
         }
     }
 
